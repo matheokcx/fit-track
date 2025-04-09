e92451b7075349ef9c530290e11f5fb3ffeeb2c5
Mathéo Deleplanque <matheo.deleplanque@gmail.com>
2025-04-09 22:45:30 +0200

    :bug: Fix a problem with the refresh of workouts on the workouts list page

--- src/app/pages/workouts-list/workouts-list.page.ts
+++ src/app/pages/workouts-list/workouts-list.page.ts
@@ -2,9 +2,10 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { StorageService } from "../../../services/storage/storage.service";
 import { Workouts } from "../../../models/workout";
 import { WorkoutItemComponent } from "../../components/workout-item/workout-item.component";
+import { WorkoutService } from "../../../services/workout/workout.service";
+import { Subscription } from "rxjs";
 
 // ==============================================
 
@@ -18,10 +19,20 @@ import { WorkoutItemComponent } from "../../components/workout-item/workout-item
 })
 export class WorkoutsListPage implements OnInit {
   protected workouts: Workouts = [];
-  private storageService: StorageService = inject(StorageService);
+  private workoutService: WorkoutService = inject(WorkoutService);
+  private sub = new Subscription();
 
   async ngOnInit() {
-    this.workouts = await this.storageService.get("workouts") || [];
+    this.loadWorkouts();
+    this.sub.add(
+      this.workoutService.onWorkoutsChange().subscribe(() => {
+        this.loadWorkouts();
+      })
+    );
+  }
+
+  async loadWorkouts(): Promise<void> {
+    this.workouts = await this.workoutService.getWorkouts();
   }
 
 }
