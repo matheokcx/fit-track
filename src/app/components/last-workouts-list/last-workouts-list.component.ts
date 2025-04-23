9a5c261460a543f1d807d7e80055ee18cccee418
Mathéo Deleplanque <matheo.deleplanque@gmail.com>
2025-04-23 22:56:35 +0200

    :hammer: Finish to refacto components on the dashboard page. Closes #44

---
+++ src/app/components/last-workouts-list/last-workouts-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { IonButton, IonIcon, IonItem, IonList, IonListHeader } from "@ionic/angular/standalone";
+import { WorkoutItemComponent } from "../workout-item/workout-item.component";
+import { Workouts } from "../../../models/workout";
+import { RouterLink } from "@angular/router";
+import { add } from "ionicons/icons";
+import { addIcons } from "ionicons";
+import { WorkoutService } from "../../../services/workout/workout.service";
+import { Subscription } from "rxjs";
+
+// ==============================================
+
+
+@Component({
+  selector: 'app-last-workouts-list',
+  templateUrl: './last-workouts-list.component.html',
+  styleUrls: ['./last-workouts-list.component.scss'],
+  imports: [IonButton, IonIcon, IonItem, IonList, IonListHeader, WorkoutItemComponent, RouterLink]
+})
+export class LastWorkoutsListComponent implements OnInit, OnDestroy {
+  protected lastWorkouts: Workouts = [];
+  protected workoutService: WorkoutService = inject(WorkoutService);
+  private sub = new Subscription();
+
+  public constructor(){
+    addIcons({add});
+  }
+
+  public async ngOnInit() {
+    await this.loadWorkouts();
+    this.sub.add(this.workoutService.onWorkoutsChange().subscribe(() => this.loadWorkouts()));
+  }
+
+  public ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  private async loadWorkouts() {
+    const workouts: Workouts = await this.workoutService.getWorkouts();
+    this.lastWorkouts = (workouts?.slice(-5, workouts.length)).reverse();
+  }
+
+}
---
+++ src/app/components/nutrition-informations-pannel/nutrition-informations-pannel.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { IonCol, IonGrid, IonIcon, IonRow } from "@ionic/angular/standalone";
+import { addIcons } from "ionicons";
+import { egg, flash, pizza, water } from "ionicons/icons";
+import { ProfileInformationsService } from "../../../services/profile-informations/profile-informations.service";
+import { Subscription } from "rxjs";
+
+// ==============================================
+
+
+@Component({
+  selector: 'app-nutrition-informations-pannel',
+  templateUrl: './nutrition-informations-pannel.component.html',
+  styleUrls: ['./nutrition-informations-pannel.component.scss'],
+  imports: [IonCol, IonGrid, IonIcon, IonRow]
+})
+export class NutritionInformationsPannelComponent implements OnInit, OnDestroy {
+  protected weight: number | null = null;
+  protected weightGoal: number | null = null;
+  protected height: number | null = null;
+  protected age: number | null = null;
+  protected profileService: ProfileInformationsService = inject(ProfileInformationsService);
+  private sub = new Subscription();
+
+  public constructor(){
+    addIcons({flash, egg, water, pizza});
+  }
+
+  public async ngOnInit() {
+    this.loadProfileInformations();
+    this.sub.add(this.profileService.onProfileChange().subscribe(() => this.loadProfileInformations()));
+  }
+
+  public ngOnDestroy() {
+    this.sub.unsubscribe();
+
+  }
+
+  private async loadProfileInformations() {
+    this.weight = await this.profileService.getWeight();
+    this.weightGoal = await this.profileService.getWeightGoal();
+    this.height = await this.profileService.getHeight();
+    this.age = await this.profileService.getAge();
+  }
+
+  public calculateCaloriesNeeds(): number | null {
+    if(!this.weight || !this.height || !this.age) return null;
+    return (10 * this.weight + 6.25 * this.height - 5 * this.age + 5) * 1.55 + 300;
+  }
+
+}
